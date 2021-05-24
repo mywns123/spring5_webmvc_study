42d2f4608cdd704e338e4c3612c9fe469bfb07a1
mywns123 <wnsduq2000@naver.com>
2021-05-24 14:06:38 +0900

    login

---
+++ src/main/java/spring5_webmvc_study/controller/AuthInfo.java
@@ -0,0 +1,27 @@
+package spring5_webmvc_study.controller;
+
+public class AuthInfo {
+
+	private long id;
+	private String email;
+	private String name;
+
+	public AuthInfo(long id, String email, String name) {
+		this.id = id;
+		this.email = email;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
